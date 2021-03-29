25ccab6c6446bf07f886c581a5ea2db6b571a6ec
massonix <ramon.massoni@cnag.crg.eu>
2021-03-29 10:20:04 +0200

    scripts cellranger

---
+++ 1-cellranger_mapping/scripts/config_vars.py
@@ -0,0 +1,7 @@
+# Config file
+
+cellranger_path = "/scratch/groups/hheyn/software/cellranger/4.0.0/cellranger"
+reference_path = "/scratch/groups/hheyn/data/reference/refdata-gex-GRCh38-2020-A"
+project_path = "/scratch/devel/rmassoni/tonsil_atlas/current/scRNA-seq/1-cellranger_mapping/"
+REPEAT_MASK_GTF = "/scratch/groups/hheyn/data/reference/GRCh38_repeat_mask/GRCh38_repeat_mask.gtf"
+REFERENCE_GTF = "/scratch/devel/rmassoni/reference/human/refdata-cellranger-GRCh38-3.0.0/genes/genes.gtf"
