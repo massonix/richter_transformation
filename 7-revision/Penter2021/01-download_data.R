60a35505f384d4285f33a6ace0b050699e8f5d3b
massonix <ramon.massoni@cnag.crg.eu>
2021-12-16 10:05:26 +0100

    QC + separate microenvironment

--- 7-revision/Penter2021/01-download_data.R
+++ 7-revision/Penter2021/01-download_data.R
@@ -4,13 +4,16 @@ library(glue)
 
 
 # Download data
-directory <- "~/Desktop/richter_transformation/data"
+# directory <- "~/Desktop/richter_transformation/data"
+directory1 <- here::here()
 gse <- getGEO("GSE165087", GSEMatrix = FALSE)
+saveRDS(gse, glue("{directory1}/results/R_objects/gse_obj.rds"))
+directory2 <- glue("{directory1}/data/Penter2021")
 gsm_list <- GSMList(gse)
 for (gsm_name in names(gsm_list)) {
   print(gsm_name)
-  download_dir <- glue("{directory}/{gsm_name}")
-  dir.create(download_dir)
+  download_dir <- glue("{directory2}/{gsm_name}")
+  dir.create(download_dir, recursive = TRUE)
   gsm <- gsm_list[[gsm_name]]
   for (i in 1:3) {
     url <- gsm@header[[glue("supplementary_file_{i}")]]
