d53af8ccc0bb092974febc983076dcfca90f176d
massonix <ramon.massoni@cnag.crg.eu>
2021-12-17 16:14:10 +0100

    dea RTvsCLL finished successfully

--- 7-revision/Penter2021/scATAC-seq/02-index_fragment_files.sh
+++ 7-revision/Penter2021/scATAC-seq/02-index_fragment_files.sh
@@ -1,12 +1,12 @@
 # This script indexes the fragments file, which is necessary for Signac
 # It also counts the number of fragments per cell so we can call cells
-# module load gcc/4.9.3-gold zlib/1.2.8 HTSLIB/latest
-dir=/scratch/devel/rmassoni/RICHTER/current/
-for i in $(ls "${dir}/data/Penter2021/scATAC-seq/"); do
+module load gcc/4.9.3-gold zlib/1.2.8 HTSLIB/latest
+dir=/scratch/devel/rmassoni/RICHTER/current/data/Penter2021/scATAC-seq
+cd $dir
+for i in $(ls); do
   echo $i;
-  gse_dir="${dir}/data/Penter2021/scATAC-seq/${i}";
-  frag_file="${gse_dir}/$(ls $gse_dir)"
-  tabix -p bed $frag_file;
-  zcat $frag_file | cut -f4 | sort | uniq -c | awk '{print $2, $1}' > "${gse_dir}/fragments_per_cell.txt"
+  frag_file=$(ls $i | grep fragments.tsv.gz$);
+  tabix -p bed "${i}/${frag_file}";
+  zcat "${i}/${frag_file}" | cut -f4 | sort | uniq -c | awk '{print $2, $1}' > "${i}/fragments_per_cell.txt"
 done
 
